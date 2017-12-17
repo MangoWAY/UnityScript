abb68ddbffd2b036aa87093eb205f7dabe73010c
MangoWAY <121424559@qq.com>
2017-12-17 11:51:48 +0800

    Update TypeWriterEffect.cs

--- typewriter/TypeWriterEffect.cs
+++ typewriter/TypeWriterEffect.cs
@@ -3,32 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class TypeWriterEffect : MonoBehaviour {
+	
     public bool isActive = false;
     private string text;
     private Text textComponent;
     private int currentPosition;
     public float internalTime = 0.5f;
+	
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
         textComponent = GetComponent<Text>();
         text = textComponent.text;
         textComponent.text = "";
 	}
 	// Update is called once per frame
-	void Update () {
-        if(isActive)
-        {
-            InvokeRepeating("writer", 0, internalTime);
-            isActive = false;
-        }
-        if(currentPosition>text.Length-1)
-        {
-            CancelInvoke();
-        }	
+	void Update () 
+	{
+		if(isActive)
+		{
+		    InvokeRepeating("writer", 0, internalTime);
+		    isActive = false;
+		}
+		if(currentPosition>text.Length-1)
+		{
+		    CancelInvoke();
+		}	
 	}
-    void writer()
-    {
+	
+   	 void writer()
+   	 {
             textComponent.text += text[currentPosition];
             currentPosition++;
-    }
+   	 }
 }
